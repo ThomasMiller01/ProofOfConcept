7d439c2fcf6bf1e088bbab8eaf4f9d0a8aedae07
Thomas <thomas.miller01.mail@gmail.com>
2019-05-15 14:20:30 +0200

    feat (empire_cellular_automaton): set init_reproduction value to variable

--- empire_cellular_automaton/person.py
+++ empire_cellular_automaton/person.py
@@ -10,6 +10,7 @@ class Person:
         self._age = age
         self._strength = strength
         self._reproductionValue = reproductionValue
+        self._old_reproductionValue = reproductionValue
         self.reproductionThreshold = 100
         self.x = x
         self.y = y
@@ -26,40 +27,57 @@ class Person:
         if neighbour_state == 'water':
             return None
         elif neighbour_state == 'empty' or neighbour_state == self._colonyName:
+            old_x = self.x
+            old_y = self.y
             self.x = neighbour[0][0]
             self.y = neighbour[0][1]
             self._map.updatePixel(self.x, self.y, self.color)
             if checkForReproduction == 'reproduction':
-                mutation = self.getMutation(self._strength)
-                return {'age': self._age, 'strength': mutation, 'reproductionValue': self._reproductionValue, 'x': self.x, 'y': self.y}
+                # mutation = self.getMutation(
+                #     self._strength, self._old_reproductionValue)
+                mutation = [self._strength, self._old_reproductionValue]
+                return {'age': self._age, 'strength': mutation[0], 'reproductionValue': mutation[1], 'x': self.x, 'y': self.y}
             else:
+                self._map.updatePixel(old_x, old_y, [0, 124, 5])
                 return None
         else:
             # fight
             return None
 
-    def getMutation(self, strength):
+    def getRndMutation(self, value):
         rnd_plus_minus = randint(0, 1)
         rnd_amount = randint(0, 10)
         if rnd_plus_minus == 0:
-            return strength + rnd_amount
+            _value = value + rnd_amount
+            if _value > 100:
+                return value
+            else:
+                return _value
         elif rnd_plus_minus == 1:
-            return strength - rnd_amount
+            _value = value - rnd_amount
+            if _value < 0:
+                return value
+            else:
+                return _value
+
+    def getMutation(self, strength, reproduction):
+        toReturn = []
+        toReturn.append(self.getRndMutation(strength))
+        toReturn.append(self.getRndMutation(reproduction))
+        return toReturn
 
     def checkFor(self, check):
         if check == 'age':
             if self._age > self._strength:
                 return 'dead'
-            rnd = randint(0, 1) * 0.2
-            self._age += rnd
+            self._age += 1
             return None
         elif check == 'reproduction':
             if self._reproductionValue > self.reproductionThreshold:
-                self._reproductionValue = 0
+                self._reproductionValue = self._old_reproductionValue
                 return 'reproduction'
             else:
-                rnd = randint(0, 1)
-                self._reproductionValue += rnd
+                self._reproductionValue += 1
                 return None
         else:
             return None
