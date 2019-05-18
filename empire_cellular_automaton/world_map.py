58c680ee338ce14b5c8254253b1d3e1f43c6f6f9
Thomas Miller <thomas.miller01.mail@gmail.com>
2019-05-18 11:45:32 +0200

    style: uncomment gettign color percentage

--- empire_cellular_automaton/world_map.py
+++ empire_cellular_automaton/world_map.py
@@ -84,18 +84,18 @@ class Map:
             return self.colorCodes[str(pixel[1].tolist())]
 
     def getColorPercentage(self, color):
-        # allColor = []
-        # # foreach pixel
-        # for y in range(0, self.h):
-        #     for x in range(0, self.w):
-        #         # if pixel matches color append pixel to allColor
-        #         if self.map_image[y, x].item(0) == color[0] and self.map_image[y, x].item(1) == color[1] and self.map_image[y, x].item(2) == color[2]:
-        #             allColor.append([[x, y], color])
-        # allColorNmb = allColor.__len__()
-        # # calculate percentage
-        # percentage = allColorNmb / self.land_pixel_nmb * 100
-        # return round(percentage)
-        return 0
+        allColor = []
+        # foreach pixel
+        for y in range(0, self.h):
+            for x in range(0, self.w):
+                # if pixel matches color append pixel to allColor
+                if self.map_image[y, x].item(0) == color[0] and self.map_image[y, x].item(1) == color[1] and self.map_image[y, x].item(2) == color[2]:
+                    allColor.append([[x, y], color])
+        allColorNmb = allColor.__len__()
+        # calculate percentage
+        percentage = allColorNmb / self.land_pixel_nmb * 100
+        return round(percentage)
+        # return 0
 
     # method used for getting x and y value of mouse click
     # def mouse_drawing(self, event, x, y, flags, params):
