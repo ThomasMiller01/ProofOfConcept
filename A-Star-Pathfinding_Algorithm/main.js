a38ddf8c3312bb019b980650d1ee6c53a853dcbb
Thomas Miller <thomas.miller01.mail@gmail.com>
2019-09-07 17:17:53 +0200

    feat: all files

--- A-Star-Pathfinding_Algorithm/main.js
+++ A-Star-Pathfinding_Algorithm/main.js
@@ -1,3 +1,146 @@
-function setup() {}
+let grid = [
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0],
+  [1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
 
-function draw() {}
+let nodeGrid = [];
+
+let openSet = [];
+let closedSet = [];
+
+let startPos = [0, 0];
+let startNode;
+let endPos = [14, 14];
+let endNode;
+
+let _path = [];
+
+let found = false;
+
+function setup() {
+  createCanvas(610, 610);
+  for (let y = 0; y < grid.length; y++) {
+    nodeGrid.push([]);
+    for (let x = 0; x < grid[0].length; x++) {
+      let currentNode = new Node(
+        { x: x, y: y },
+        grid[y][x] == 0 ? true : false
+      );
+      nodeGrid[y].push(currentNode);
+      if (currentNode.x == startPos[0] && currentNode.y == startPos[1]) {
+        currentNode.g = 0;
+        currentNode.h = 0;
+      }
+      openSet.push(currentNode);
+    }
+  }
+  startNode = nodeGrid[startPos[1]][startPos[0]];
+  endNode = nodeGrid[endPos[1]][endPos[0]];
+}
+
+function draw() {
+  background(255);
+  for (let y = 0; y < nodeGrid.length; y++) {
+    for (let x = 0; x < nodeGrid[0].length; x++) {
+      nodeGrid[y][x].draw("normal");
+    }
+  }
+  closedSet.forEach(node => {
+    node.draw("visited");
+  });
+  if (openSet.length != 0 || found) {
+    let currentNode = openSet[0];
+    for (let i = 0; i < openSet.length; i++) {
+      if (
+        openSet[i].f() < currentNode.f() ||
+        (openSet[i] == currentNode.f() && openSet[i].h < currentNode.h)
+      ) {
+        currentNode = openSet[i];
+      }
+    }
+    currentNode.draw("current");
+    openSet.splice(openSet.indexOf(currentNode), 1);
+    closedSet.push(currentNode);
+    if (currentNode == endNode) {
+      found = true;
+    } else {
+      let neighbours = getNeighbours(currentNode);
+      neighbours.forEach(neighbour => {
+        if (neighbour.walkable && !closedSet.includes(neighbour)) {
+          neighbour.g = currentNode.g + getDist(currentNode, neighbour);
+          neighbour.h = getDist(neighbour, endNode);
+          neighbour.parent = currentNode;
+
+          if (openSet.includes(neighbour)) {
+            if (neighbour.h <= currentNode.g) {
+              openSet.push(neighbour);
+            }
+          }
+        }
+      });
+    }
+  }
+  if (found) {
+    retracePath(startNode, endNode);
+    _path.forEach(node => {
+      node.draw("path");
+    });
+    noLoop();
+  }
+}
+
+function retracePath(startNode, endNode) {
+  let currentNode = endNode;
+  while (currentNode != startNode) {
+    _path.push(currentNode);
+    currentNode = currentNode.parent;
+  }
+  _path.reverse();
+}
+
+function getDist(nodeA, nodeB) {
+  let distX = Math.abs(nodeA.x - nodeB.x);
+  let distY = Math.abs(nodeA.y - nodeB.y);
+
+  if (distX > distY) {
+    return 14 * distY + 10 * (distX - distY);
+  } else {
+    return 14 * distX + 10 * (distY - distX);
+  }
+}
+
+function getNeighbours(node) {
+  let neighbours = [];
+  for (let x = -1; x <= 1; x++) {
+    for (let y = -1; y <= 1; y++) {
+      if (x == 0 && y == 0) {
+        continue;
+      }
+      let checkX = node.x + x;
+      let checkY = node.y + y;
+
+      if (
+        checkX >= 0 &&
+        checkX < nodeGrid[0].length &&
+        checkY >= 0 &&
+        checkY < nodeGrid.length
+      ) {
+        neighbours.push(nodeGrid[checkY][checkX]);
+      }
+    }
+  }
+  return neighbours;
+}
--- A-Star-Pathfinding_Algorithm/node.js
+++ A-Star-Pathfinding_Algorithm/node.js
@@ -1,6 +1,60 @@
 class Node {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
+  constructor(pos, walkable) {
+    this.x = pos.x;
+    this.y = pos.y;
+    this.walkable = walkable;
+    this.width = 40;
+    this.g = Infinity;
+    this.h = Infinity;
+    this.parent = null;
+  }
+
+  f() {
+    return this.g + this.h;
+  }
+
+  draw(state) {
+    switch (state) {
+      case "normal":
+        if (this.walkable) {
+          stroke("black");
+          fill("white");
+        } else {
+          stroke("black");
+          fill("red");
+        }
+        break;
+      case "current":
+        stroke("black");
+        fill("blue");
+        break;
+      case "visited":
+        stroke("black");
+        fill("grey");
+        break;
+      case "path":
+        stroke("black");
+        fill("yellow");
+        break;
+      default:
+        break;
+    }
+    rect(this.x * this.width, this.y * this.width, this.width, this.width);
+    fill("black");
+    text(
+      this.f() != Infinity ? this.f() : "-",
+      this.x * this.width + 3,
+      this.y * this.width + 13
+    );
+    text(
+      this.g != Infinity ? this.g : "-",
+      this.x * this.width + 3,
+      this.y * this.width + this.width - 3
+    );
+    text(
+      this.h != Infinity ? this.h : "-",
+      this.x * this.width + this.width - 12,
+      this.y * this.width + this.width - 3
+    );
   }
 }
