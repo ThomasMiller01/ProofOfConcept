ad3dd8e61ff21ccb8dd8cfc21080a96385da4a7e
Thomas Miller <thomas.miller01.mail@gmail.com>
2020-02-04 14:18:13 +0100

    feat: statistics

--- empire_cellular_automaton/statistics/dataset_processing.py
+++ empire_cellular_automaton/statistics/dataset_processing.py
@@ -5,6 +5,97 @@ import numpy as np
 import os
 
 
+def people_distribution_map(data, file):
+    plt_size_x = int(np.ceil(np.sqrt(len(data['data']))))
+    plt_size_y = int(np.ceil(np.sqrt(len(data['data'])) - 0.5))
+    fig, axs = plt.subplots(plt_size_x, plt_size_y, figsize=(10, 10))
+    fig.suptitle("people distribution", fontsize=10)
+    fig.tight_layout(pad=3.0)
+    i = 0
+    for ax_s in axs:
+        for ax in ax_s:
+            if i < len(data['data']):
+                gen = data['data'][i]
+                minified_data = [y['colonies']['0']['people']
+                                 for y in gen['data']]
+                x_y_data = []
+                for day in minified_data:
+                    for person in day:
+                        x_y_data.append((person['x'], person['y']))
+
+                x_y_not_duplicated = []
+                x_y_not_duplicated_data = []
+                for x_y in x_y_data:
+                    try:
+                        index = x_y_not_duplicated.index(x_y)
+                    except:
+                        index = None
+                    if index == None:
+                        x_y_not_duplicated.append(x_y)
+                        x_y_not_duplicated_data.append(1)
+                    else:
+                        x_y_not_duplicated_data[index] += 1
+
+                x, y = zip(*x_y_not_duplicated)
+
+                s = np.asarray([x for x in x_y_not_duplicated_data])
+
+                img = plt.imread("map.jpg")
+
+                ax.scatter(x, y, s=s)
+                ax_xlim = ax.get_xlim()
+                ax_ylim = ax.get_ylim()
+                ax.imshow(img, origin="lower")
+                ax.set_xlim(ax_xlim)
+                ax.set_ylim(ax_ylim[::-1])
+                ax.set(title="gen " + str(gen['gen']))
+                i += 1    
+    plt.savefig(file)
+    plt.close(fig=fig)
+
+
+def kind_of_disease_per_generation(data, file):
+    plt_size_x = int(np.ceil(np.sqrt(len(data['data']))))
+    plt_size_y = int(np.ceil(np.sqrt(len(data['data'])) - 0.5))
+    fig, axs = plt.subplots(plt_size_x, plt_size_y, figsize=(10, 10))
+    fig.suptitle("kind of disease", fontsize=16)
+    fig.tight_layout(pad=3.0)
+    i = 0
+    for ax_s in axs:
+        for ax in ax_s:
+            if i < len(data['data']):
+                gen = data['data'][i]
+                minified_data = [y['colonies']['0']['people']
+                                 for y in gen['data']]
+                diseased_people = []
+                for day in minified_data:
+                    for person in day:
+                        if person['_disease'] != None:
+                            diseased_people.append(person)
+                disease_num = {}
+                for person in diseased_people:
+                    if person['_disease']['kind'][0] not in disease_num:
+                        disease_num[person['_disease']['kind'][0]] = 1
+                    else:
+                        disease_num[person['_disease']['kind'][0]] += 1
+                y_list_data = []
+                y_list_labels = []
+                for kind in disease_num:
+                    y_list_data.append(disease_num[kind])
+                    y_list_labels.append(kind)
+                x = np.arange(0, len(y_list_data))
+                y = np.asarray(y_list_data)
+
+                ax.bar(x, y)
+                ax.set_xticks(x)
+                ax.set_yticks(y)
+                ax.set_xticklabels(y_list_labels)
+                ax.set(title="gen " + str(gen['gen']))
+                i += 1
+    plt.savefig(file)
+    plt.close(fig=fig)
+
+
 def strength_distribution_per_generation(data, file):
     plt_size_x = int(np.ceil(np.sqrt(len(data['data']))))
     plt_size_y = int(np.ceil(np.sqrt(len(data['data'])) - 0.5))
@@ -34,6 +125,7 @@ def strength_distribution_per_generation(data, file):
                 ax.grid()
                 i += 1
     plt.savefig(file)
+    plt.close(fig=fig)
 
 
 def population_pyramid_per_generation(data, file):
@@ -65,6 +157,7 @@ def population_pyramid_per_generation(data, file):
                 ax.grid()
                 i += 1
     plt.savefig(file)
+    plt.close(fig=fig)
 
 
 def disease_over_time(data, file):
@@ -80,6 +173,7 @@ def disease_over_time(data, file):
     ax.grid()
     plt.legend(loc="best", title="generation")
     plt.savefig(file)
+    plt.close(fig=fig)
 
 
 def avg_reproductionValue_over_time(data, file):
@@ -95,6 +189,7 @@ def avg_reproductionValue_over_time(data, file):
     ax.grid()
     plt.legend(loc="best", title="generation")
     plt.savefig(file)
+    plt.close(fig=fig)
 
 
 def avg_age_over_time(data, file):
@@ -110,6 +205,7 @@ def avg_age_over_time(data, file):
     ax.grid()
     plt.legend(loc="best", title="generation")
     plt.savefig(file)
+    plt.close(fig=fig)
 
 
 def population_over_time(data, file):
@@ -125,11 +221,11 @@ def population_over_time(data, file):
     ax.grid()
     plt.legend(loc="best", title="generation")
     plt.savefig(file)
+    plt.close(fig=fig)
 
 
-if __name__ == "__main__":
-    dataset_name = "dataset_04"
-
+def save_figs(dataset_name):
+    print("saving " + dataset_name + " ...")
     file_name = 'statistics/' + dataset_name
 
     # load data
@@ -151,6 +247,10 @@ if __name__ == "__main__":
         data, file_name + "/pdf/population_pyramid_per_generation.pdf")
     strength_distribution_per_generation(
         data, file_name + "/pdf/strength_distribution_per_generation.pdf")
+    kind_of_disease_per_generation(
+        data, file_name + "/pdf/kind_of_disease.pdf")
+    people_distribution_map(
+        data, file_name + "/pdf/people_distribution_map.pdf")
 
     # save as png
     try:
@@ -167,3 +267,15 @@ if __name__ == "__main__":
         data, file_name + "/png/population_pyramid_per_generation.png")
     strength_distribution_per_generation(
         data, file_name + "/png/strength_distribution_per_generation.png")
+    kind_of_disease_per_generation(
+        data, file_name + "/png/kind_of_disease.png")
+    people_distribution_map(
+        data, file_name + "/png/people_distribution_map.png")
+
+
+if __name__ == "__main__":
+    save_figs("dataset_01")
+    save_figs("dataset_02")
+    save_figs("dataset_03")
+    save_figs("dataset_04")
+    print("all saved ...")
